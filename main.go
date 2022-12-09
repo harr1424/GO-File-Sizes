f61fde7065f6c60a9fe7774a7c5334a7069829cd
John Harrington <harrington59802@protonmail.com>
2022-12-09 10:39:09 -0700

    worked on walk implementation

--- main.go
+++ main.go
@@ -18,13 +18,35 @@ func main() {
 	fullPath := args[1]
 	dirMap := make(map[string]uint32)
 
+	// dirMap should be passed here
 	filepath.WalkDir(fullPath, walk)
 
 }
 
-func walk(path string, dir fs.DirEntry, err error) error {
+// how to create paramter accepting &map[string]uint32 ?
+func walk(path string, entry fs.DirEntry, err error, directoryMap map[string]uint32) error {
 	if err != nil {
 		return err
 	}
 
+	if !entry.IsDir() {
+		// add entry and it's size to dirMap
+
+		file, err := os.Stat(path)
+		if err != nil {
+			log.Println("Error determining file size:", err)
+		}
+
+		size := file.Size()
+		directoryMap[path] = uint32(size)
+
+	} else if entry.IsDir() {
+		// recurse here
+
+	} else {
+		log.Println("Unhandled branch:")
+		log.Println(entry.Info())
+	}
+
+	return nil
 }
