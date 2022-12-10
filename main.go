d8dd8708d5abba232568f556b9f27a2a0803dc00
harr1424 <harrington59802@protonmail.com>
2022-12-09 18:17:14 -0700

    finalized mini-project and added comments

--- main.go
+++ main.go
@@ -6,7 +6,17 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 )
+
+/*
+A struct to hold a key value pair corresponding to single member of a
+map[string]uint32. This will be sued to sort the map data by value.
+*/
+type pair struct {
+	name string
+	size uint32
+}
 
 func main() {
 	args := os.Args
@@ -18,34 +28,52 @@ func main() {
 	fullPath := args[1]
 	dirMap := make(map[string]uint32)
 
-	// dirMap should be passed here
-	filepath.WalkDir(fullPath, walk)
+	// Recursively visit all filesystem entries at the provided path
+	filepath.WalkDir(fullPath, func(path string, entry fs.DirEntry, err error) error {
+		return walk(path, entry, err, dirMap)
+	})
+
+	// Declare a slice of pairs
+	var pairs []pair
+
+	// Add map entries to pairs slice
+	for k, v := range dirMap {
+		pairs = append(pairs, pair{k, v})
+	}
+
+	// Sort descending
+	sort.Slice(pairs, func(a, b int) bool {
+		return pairs[a].size > pairs[b].size
+	})
+
+	// If the provided path had fewer than 10 entries, print all entries and their sizes in bytes
+	if len(pairs) < 10 {
+		for _, file := range pairs {
+			fmt.Printf("%s, %d\n", file.name, file.size)
+		}
+	} else { // print only the largest 10 entries and their sizes in bytes
+		for i := 0; i < 10; i++ {
+			fmt.Printf("%s:  %d\n", pairs[i].name, pairs[i].size)
+		}
+	}
 
 }
 
-// how to create paramter accepting &map[string]uint32 ?
-func walk(path string, entry fs.DirEntry, err error, directoryMap map[string]uint32) error {
+// Implement a WalkDirFunc that accepts a reference to a map[string]uint32
+func walk(path string, entry fs.DirEntry, err error, dirMap map[string]uint32) error {
 	if err != nil {
 		return err
 	}
 
 	if !entry.IsDir() {
 		// add entry and it's size to dirMap
-
 		file, err := os.Stat(path)
 		if err != nil {
 			log.Println("Error determining file size:", err)
 		}
 
 		size := file.Size()
-		directoryMap[path] = uint32(size)
-
-	} else if entry.IsDir() {
-		// recurse here
-
-	} else {
-		log.Println("Unhandled branch:")
-		log.Println(entry.Info())
+		dirMap[path] = uint32(size)
 	}
 
 	return nil
